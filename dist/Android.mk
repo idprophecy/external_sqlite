129e130e01c699f5ad8c43b2d6505bce82d3eb93
Martin Otto <paraboloid234@gmail.com>
2017-07-03 17:22:21 +0200

    TEST

--- dist/Android.mk
+++ dist/Android.mk
@@ -47,6 +47,10 @@ LOCAL_SRC_FILES := $(common_src_files)
 
 LOCAL_CFLAGS += $(device_sqlite_flags)
 
+LOCAL_SDCLANG := false
+
+LOCAL_CLANG := false
+
 LOCAL_SHARED_LIBRARIES := libdl
 
 LOCAL_MODULE:= libsqlite
@@ -87,6 +91,10 @@ ifneq ($(SDK_ONLY),true)  # SDK doesn't need device version of sqlite3
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := shell.c
+
+LOCAL_SDCLANG := false
+
+LOCAL_CLANG := false
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../android \
