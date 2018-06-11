ce30cdbf0e834239ae88b2b0598914137662ac6a
Martin Otto <paraboloid234@gmail.com>
2018-06-11 21:04:19 +0200

    Revert "TEST #2"
    This reverts commit 661614a218c1c4c5f3dc3a57405aa8cb54607d44.

--- dist/Android.mk
+++ dist/Android.mk
@@ -47,7 +47,9 @@ LOCAL_SRC_FILES := $(common_src_files)
 
 LOCAL_CFLAGS += $(device_sqlite_flags)
 
-LOCAL_SDCLANG_LTO := true
+LOCAL_SDCLANG := false
+
+LOCAL_CLANG := false
 
 LOCAL_SHARED_LIBRARIES := libdl
 
@@ -90,7 +92,9 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := shell.c
 
-LOCAL_SDCLANG_LTO := true
+LOCAL_SDCLANG := false
+
+LOCAL_CLANG := false
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../android \
